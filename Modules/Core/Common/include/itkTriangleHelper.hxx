fa7389f46c71643699c3ea6058245f16045999c0
Alexander Leinoff <ajjleinoff@gmail.com>
2015-04-28 01:01:08 -0500

    WIP: Replaces Floating Point
    Equality with itk::Math::FloatAlmostEquals
    in the file:
    
    /Users/ajjl/dev/itk/ITK/Modules/Core/Common/include/itkTriangleHelper.hxx
    
    Sometimes this causes other warnings/Errors.
    
    Change-Id: Ia59d85c691a27a5a2ce62ecf53a3c00820d7a811

--- Modules/Core/Common/include/itkTriangleHelper.hxx
+++ Modules/Core/Common/include/itkTriangleHelper.hxx
@@ -19,6 +19,7 @@
 #define itkTriangleHelper_hxx
 
 #include "itkTriangleHelper.h"
+#include "itkMath.h"
 
 namespace itk
 {
@@ -81,14 +82,14 @@ TriangleHelper< TPoint >::Cotangent(const PointType & iA,
 
   CoordRepType v21_l2 = v21.GetSquaredNorm();
 
-  if ( v21_l2 != NumericTraits< CoordRepType >::ZeroValue() )
+  if ( !itk::Math::FloatAlmostEqual(v21_l2 , NumericTraits< CoordRepType >::ZeroValue()) )
     {
     v21 /= std::sqrt(v21_l2);
     }
 
   VectorType   v23 = iC - iB;
   CoordRepType v23_l2 = v23.GetSquaredNorm();
-  if ( v23_l2 != NumericTraits< CoordRepType >::ZeroValue() )
+  if ( !itk::Math::FloatAlmostEqual(v23_l2 , NumericTraits< CoordRepType >::ZeroValue()) )
     {
     v23 /= std::sqrt(v23_l2);
     }
