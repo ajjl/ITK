198d3daad03ab15e323d5fb89f44da853fea075d
Alexander Leinoff <ajjleinoff@gmail.com>
2015-05-03 17:45:28 -0500

    COMP: Replaces Floating Point
    Equality with itk::Math::EqualsComparisonCaller
    in the file:
    
    Modules/Core/Common/test/itkImageIteratorWithIndexTest.cxx
    
    Change-Id: I464d925d4be36d1f0dbba3e83455cce98a4a4e61

--- Modules/Core/Common/test/itkImageIteratorWithIndexTest.cxx
+++ Modules/Core/Common/test/itkImageIteratorWithIndexTest.cxx
@@ -20,6 +20,7 @@
 
 #include "itkImageRegionIteratorWithIndex.h"
 #include "itkNumericTraits.h"
+#include "itkMath.h"
 
 
 template <typename TPixelType>
@@ -65,7 +66,7 @@ class itkImageIteratorWithIndexTestIteratorTester
        PixelType value = it.Get();
        PixelType testValue = value * static_cast<typename itk::NumericTraits<PixelType>::ValueType>( 2 );
        it.Set( testValue);
-       if( it.Get() != testValue )
+       if( itk::Math::NotEqualsComparison(it.Get() , testValue) )
         {
         std::cerr << "TestIterator failed!" << std::endl;
         return false;
@@ -89,7 +90,7 @@ class itkImageIteratorWithIndexTestIteratorTester
      while( !it.IsAtEnd() )
        {
        PixelType value = it.Get();
-       if( value != it.Get() ) // check repeatibility
+       if( itk::Math::NotEqualsComparison(value , it.Get()) ) // check repeatibility
          {
          std::cerr << "TestConstIterator failed!" << std::endl;
          return false;
@@ -113,7 +114,7 @@ class itkImageIteratorWithIndexTestIteratorTester
      while( !it.IsAtReverseEnd() )
        {
        PixelType value = it.Get();
-       if( value != it.Get() ) // check repeatibility
+       if( itk::Math::NotEqualsComparison(value , it.Get()) ) // check repeatibility
          {
          std::cerr << "TestReverseIteration failed!" << std::endl;
          return false;
