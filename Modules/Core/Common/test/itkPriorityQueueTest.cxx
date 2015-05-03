b2d1fb23711e91338ba103e7d0513f4815f08e59
Alexander Leinoff <ajjleinoff@gmail.com>
2015-05-03 17:45:31 -0500

    COMP: Replaces Floating Point
    Equality with itk::Math::EqualsComparisonCaller
    in the file:
    
    Modules/Core/Common/test/itkPriorityQueueTest.cxx
    
    Change-Id: Ic15d3c9535f45c71d316fc84733435b43a2f0c16

--- Modules/Core/Common/test/itkPriorityQueueTest.cxx
+++ Modules/Core/Common/test/itkPriorityQueueTest.cxx
@@ -16,6 +16,7 @@
  *
  *=========================================================================*/
 #include "itkPriorityQueueContainer.h"
+#include "itkMath.h"
 
 int itkPriorityQueueTest( int, char * [] )
 {
@@ -64,7 +65,7 @@ int itkPriorityQueueTest( int, char * [] )
   std::cout <<"Min Priority Queue   ";
   while( !min_priority_queue->Empty() )
     {
-    if( min_priority_queue->Peek().m_Priority != *it )
+    if( itk::Math::NotEqualsComparison(min_priority_queue->Peek().m_Priority , *it) )
       {
       std::cout <<min_priority_queue->Peek().m_Priority <<" " <<*it <<std::endl;
       return EXIT_FAILURE;
@@ -83,7 +84,7 @@ int itkPriorityQueueTest( int, char * [] )
   std::cout <<"Max Priority Queue   ";
   while( !max_priority_queue->Empty() )
     {
-    if( max_priority_queue->Peek().m_Priority != sequence.back() )
+    if( itk::Math::NotEqualsComparison(max_priority_queue->Peek().m_Priority , sequence.back()) )
       {
       std::cout <<max_priority_queue->Peek().m_Priority <<" " <<sequence.back() <<std::endl;
       return EXIT_FAILURE;
